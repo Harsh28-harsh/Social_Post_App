c1790df50a37d62165e440ca2d755c3df13c6b80
Harsh28-harsh <harshjacker921@gmail.com>
2026-06-23 22:21:52 +0530

    Fixed route import

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
 
-const connectToMongo = require('./db');
-const cors = require('cors');
-const express = require('express');
+const connectToMongo = require("./db");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 connectToMongo();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/posts', require('./routes/post'));
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/Post"));
 
 app.listen(port, () => {
   console.log(`Server Running On Port ${port}`);
